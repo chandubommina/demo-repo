cfd138c49a3dbfc591db135c9b63a69564b712d2
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-01-10 07:58:05 +0530

    made changes to code

--- file1.py
+++ file1.py
@@ -1,11 +1,11 @@
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name = input("name: ")
     house = input("house: ")
-    return name, house
+    return (name, house)
 
 if __name__ == '__main__':
     main()
