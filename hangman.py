950c0bef63ede17fa73dc150bab908373f01550f
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-04-22 23:05:41 +0530

    part 1

---
+++ hangman.py
@@ -0,0 +1,21 @@
+import random
+words = ['anna','pope','mime','chandu','abacabac']
+word = random.choice(words)
+print(word)
+blanks = []
+for i in range(len(word)):
+    blanks.append('_ ')
+print(blanks)
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input().lower()
+    for position in range(len(word)):
+        letter = word[position]
+        if letter == guess:
+            blanks[position] = letter
+            print(blanks)
+    if '_ ' not in blanks:
+        end_of_game = True
+        print(blanks)
