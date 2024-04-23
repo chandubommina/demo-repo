808b402f1fb3fe4cc0a13206de3c44036bf29987
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-04-23 08:13:21 +0530

    changes

--- hangman.py
+++ hangman.py
@@ -5,21 +5,24 @@ print(word)
 blanks = []
 for i in range(len(word)):
     blanks.append('_ ')
-print(blanks)
-
+print(f"{' '.join(blanks)}")
+lives = 5
 end_of_game = False
-
 while not end_of_game:
     guess = input().lower()
     for position in range(len(word)):
         letter = word[position]
         if letter == guess:
             blanks[position] = letter
-            print(blanks)
+            print(f"{' '.join(blanks)}")
+            
+    if guess not in word:
+        lives = lives-1
+        if lives == 0:
+            end_of_game = True
+            print('you lost')
+            print(f'word is {word}')
     if '_ ' not in blanks:
         end_of_game = True
-        print(blanks)
-
-
-
-        
+        print('you win')
+        print(f"{' '.join(blanks)}")
