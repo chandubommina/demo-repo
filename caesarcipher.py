81d91bd30abc8450195b4800e9e9c71e54c04725
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-04-26 09:27:47 +0530

    changes

--- caesarcipher.py
+++ caesarcipher.py
@@ -3,17 +3,24 @@ direction = input('Type "encode" to encode and "decode" to decode \n').lower()
 text = input('Type ur message \n')
 shift = int(input('enter shift number \n'))
 
+#to handle any shift lenght
+shift = shift % 26
+
 def caesar(text,shift,direction):
     caesar_text = ''
     for i  in text:
-        position = alphabets.index(i)
-        if direction == 'encode':
-            new_position = position+shift
-            new_letter = alphabets[new_position]
-            caesar_text += new_letter
+        #to handle symbols,numbers,spaces,etc
+        if i in alphabets:
+            position = alphabets.index(i)
+            if direction == 'encode':
+                new_position = position+shift
+                new_letter = alphabets[new_position]
+                caesar_text += new_letter
+            else:
+                new_position = position-shift
+                new_letter = alphabets[new_position]
+                caesar_text += new_letter
         else:
-            new_position = position-shift
-            new_letter = alphabets[new_position]
-            caesar_text += new_letter            
+            caesar_text += i
     print(caesar_text)
 caesar(text,shift,direction) 
