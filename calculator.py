37280e851c5c455a45ea867125ddaa36efd5fb53
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-04-28 11:16:13 +0530

    final changes

--- calculator.py
+++ calculator.py
@@ -15,20 +15,21 @@ operations = {
     "/" : div
 }
 
-flag = True
-first = int(input("enter first number : "))
-for i in operations:
-    print(i)
-while flag:
-    op = input("pick an operation : ")
-    second = int(input("enter next number : "))
-    cal_function = operations[op]
-    result = cal_function(first, second)
-    print(f"{first} {op} {second} is {result}")
-    again = input(f"type 'y' to continue with {result},or type 'n' to exit..").lower()
-    if again == 'n':
-        flag = False
-        print("bye!")
-    else:
-        first = result
-
+def calculator():
+    flag = True
+    first = float(input("enter first number : "))
+    for i in operations:
+        print(i)
+    while flag:
+        op = input("pick an operation : ")
+        second = float(input("enter next number : "))
+        cal_function = operations[op]
+        result = cal_function(first, second)
+        print(f"{first} {op} {second} is {result}")
+        again = input(f"type 'y' to continue with {result},or type 'n' to new start : ").lower()
+        if again == 'n':
+            flag = False
+            calculator()
+        else:
+            first = result
+calculator()
