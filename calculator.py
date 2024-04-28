ed15143129f5e0afb0e2119b4c65d984946996db
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-04-28 10:38:25 +0530

    improved cal

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ def sub(a,b):
 def mul(a,b):
     return a*b
 def div(a,b):
-    return a//b
+    return round(a/b,2)
 
 operations = {
     "+" : add,
@@ -15,12 +15,20 @@ operations = {
     "/" : div
 }
 
+flag = True
 first = int(input("enter first number : "))
-second = int(input("enter second number : "))
 for i in operations:
     print(i)
-op = input("choose operation : ")
-cal_function = operations[op]
-result = cal_function(first, second)
-print(f"{first} {op} {second} is {result}")
+while flag:
+    op = input("pick an operation : ")
+    second = int(input("enter next number : "))
+    cal_function = operations[op]
+    result = cal_function(first, second)
+    print(f"{first} {op} {second} is {result}")
+    again = input(f"type 'y' to continue with {result},or type 'n' to exit..").lower()
+    if again == 'n':
+        flag = False
+        print("bye!")
+    else:
+        first = result
 
