fcc4c4c2f829b8080be403c10492bdc3415777ca
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-05-01 09:44:04 +0530

    modified

--- guessthenumber.py
+++ guessthenumber.py
@@ -1,41 +1,43 @@
 import random
 
-print("welcome to number guessing game")
-print("i m thinking of number between 1 to 100.")
+EASY_LEVEL_TURNS = 10
+HARDL_LEVEL_TURNS = 5
 
-number = random.randint(1,100)
-
-level = input("choose difficulty level! easy,hard : ")
-
-if level == 'hard':
-    attempts = 5
-else:
-    attempts = 10
+def check_answer(number,guess,turns):
+    if guess > number :
+        print("to high !")
+        return turns-1
+    elif guess < number :
+        print("to low !")
+        return turns-1
+    else:
+        print(f"you got it ! number is {number}")
+        
+def set_difficulty():
+    level = input("choose difficulty. hard or easy ? ").lower()
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARDL_LEVEL_TURNS
     
-print(f"you have {attempts} attempts to guess the number ")
-
-def compare(x,y):
-    global attempts
-    if x == y:
-        print(f"won number is {number}")
-        attempts = 0
-    elif x > y :
-        attempts -= 1
-        if attempts >0:
-            print("too high! guess again")
-            print(f"you have {attempts} attempts remaining")
-        else:
-            print(f"u lost! number is {number}")
-            print(f"you have {attempts} attempts remaining")            
-    elif x < y :
-        attempts -= 1
-        if attempts > 0:
-            print("too low! guess again")
-            print(f"you have {attempts} attempts remaining")
-        else:
-            print(f"u lost! number is {number}")
-            print(f"you have {attempts} attempts remaining")
     
-while attempts > 0:
-    guess_number = int(input("Make a guess : "))
-    compare(guess_number,number)    
+def game():    
+    
+    print("welcome number guessing game!")
+    print("i am thinking of number between 1 and 100 .")
+    number = random.randint(1,100)
+    print(f"number {number}")
+    turns = set_difficulty()
+    guess = 0
+
+    while guess != number:
+        print(f"u have {turns} attempts remaining to guess the number")
+        guess = int(input("make a guess : "))
+        turns = check_answer(number,guess,turns)
+        if turns == 0:
+            print("u ran out of guesses, u lose.")
+            return
+        elif guess != number :
+            print("guess again.")
+
+game()        
