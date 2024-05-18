6b2b491cafdcb53c65e323da511f2460e3c3197d
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-05-18 07:13:52 +0530

    sketch

--- pythonProjectDay19/main.py
+++ pythonProjectDay19/main.py
@@ -10,13 +10,18 @@ def move_backwards():
     tim.backward(10)
 
 def turn_left():
-    tim.left(90)
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
 
 def turn_right():
-    tim.right(90)
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
 
-def clear_screen():
-    my_screen.clearscreen()
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 
 my_screen.listen()
@@ -24,6 +29,6 @@ my_screen.onkey(fun=move_forwards, key="w")
 my_screen.onkey(fun=move_backwards, key="s")
 my_screen.onkey(fun=turn_right, key="a")
 my_screen.onkey(fun=turn_left, key="d")
-my_screen.onkey(fun=clear_screen, key="c")
+my_screen.onkey(fun=clear, key="c")
 my_screen.exitonclick()
 
