8e85d52c29210ef865799f4236b049c01b67d226
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-05-19 09:59:21 +0530

    created snake class

--- pythonProjectSnakeGame/main.py
+++ pythonProjectSnakeGame/main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 my_screen = Screen()
@@ -7,28 +8,14 @@ my_screen.bgcolor("black")
 my_screen.title("SnaKe Game")
 my_screen.tracer(0)
 
-positions = [(0, 0), (-20, 0), (-40, 0)]
-boxs = []
-
-for position in positions:
-    box = Turtle("square")
-    box.color("white")
-    box.penup()
-    box.goto(position)
-    boxs.append(box)
-
+snake = Snake()
 
 is_game_on = True
 while is_game_on:
     my_screen.update()
     time.sleep(0.1)
-    for box_num in range(len(boxs)-1, 0, -1):
-        next_x = boxs[box_num-1].xcor()
-        next_y = boxs[box_num-1].ycor()
-        boxs[box_num].goto(next_x, next_y)
-    boxs[0].forward(20)
-    boxs[0].left(90)
-    time.sleep(1)
+    snake.move()
+
 
 
 
---
+++ pythonProjectSnakeGame/snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    def __init__(self):
+        self.boxs = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            box = Turtle("square")
+            box.color("white")
+            box.penup()
+            box.goto(position)
+            self.boxs.append(box)
+
+    def move(self):
+
+        for box_num in range(len(self.boxs)-1, 0, -1):
+            next_x = self.boxs[box_num-1].xcor()
+            next_y = self.boxs[box_num-1].ycor()
+            self.boxs[box_num].goto(next_x, next_y)
+        self.boxs[0].forward(MOVE_DISTANCE)
+
