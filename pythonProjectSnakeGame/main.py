3e692281f8ba668bdb83bc557da1fa126ca8f04b
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-05-19 10:27:39 +0530

    controlling snake body by creating events

--- pythonProjectSnakeGame/main.py
+++ pythonProjectSnakeGame/main.py
@@ -10,22 +10,17 @@ my_screen.tracer(0)
 
 snake = Snake()
 
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.right, "Right")
+my_screen.onkey(snake.left, "Left")
+
 is_game_on = True
 while is_game_on:
     my_screen.update()
     time.sleep(0.1)
     snake.move()
-
-
-
-
-
-
-
-
-
-
-
 
 
 my_screen.exitonclick()
--- pythonProjectSnakeGame/snake.py
+++ pythonProjectSnakeGame/snake.py
@@ -18,10 +18,21 @@ class Snake:
             self.boxs.append(box)
 
     def move(self):
-
         for box_num in range(len(self.boxs)-1, 0, -1):
             next_x = self.boxs[box_num-1].xcor()
             next_y = self.boxs[box_num-1].ycor()
             self.boxs[box_num].goto(next_x, next_y)
         self.boxs[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.boxs[0].setheading(90)
+
+    def down(self):
+        self.boxs[0].setheading(270)
+
+    def right(self):
+        self.boxs[0].setheading(0)
+
+    def left(self):
+        self.boxs[0].setheading(180)
 
