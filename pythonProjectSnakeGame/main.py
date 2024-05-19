c54d0ce74eae01631b30dec9446641f52c04c515
chandubommina <155243250+chandubommina@users.noreply.github.com>
2024-05-19 09:24:00 +0530

    moving snake body

--- pythonProjectSnakeGame/main.py
+++ pythonProjectSnakeGame/main.py
@@ -1,15 +1,40 @@
 from turtle import Screen, Turtle
+import time
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.bgcolor("black")
 my_screen.title("SnaKe Game")
+my_screen.tracer(0)
 
-for i in range(3):
-    box = Turtle()
+positions = [(0, 0), (-20, 0), (-40, 0)]
+boxs = []
+
+for position in positions:
+    box = Turtle("square")
     box.color("white")
-    box.shape("square")
-    box.goto(x=0-(i*20), y=0)
+    box.penup()
+    box.goto(position)
+    boxs.append(box)
+
+
+is_game_on = True
+while is_game_on:
+    my_screen.update()
+    time.sleep(0.1)
+    for box_num in range(len(boxs)-1, 0, -1):
+        next_x = boxs[box_num-1].xcor()
+        next_y = boxs[box_num-1].ycor()
+        boxs[box_num].goto(next_x, next_y)
+    boxs[0].forward(20)
+    boxs[0].left(90)
+    time.sleep(1)
+
+
+
+
+
+
 
 
 
